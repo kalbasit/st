466b36a0315613783c7711e8763073daf81d81df
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2014-08-27 17:59:23 -0700

    13px is more visible

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Monospace:pixelsize=12:antialias=true:hinting=true";
+static char font[] = "Monospace:pixelsize=13:antialias=true:hinting=true";
 static int borderpx = 2;
 static char shell[] = "/bin/sh";
 
